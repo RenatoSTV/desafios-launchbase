5ab78b0e6ed96608c54874bd5904bff5df37bcbe
Renato ST <renatostv@gmail.com>
2020-06-19 22:21:28 -0300

    pagina do curso
    - reutilização de codigo pelo nunjucks
    
    - requisição de dados dinamicamente
    
    -página para exibição dos cursos individualmente

---
+++ desafio3-1/data.js
@@ -0,0 +1,23 @@
+module.exports=[
+    {
+        id: "starter",
+        image: "https://skylab.rocketseat.com.br/static/64c237ccff807c054339a62d53b4b402.svg",
+        title: "Starter",
+        modules:"5 Módulos",
+        price:"FREE"
+    },
+    {
+        id: "launchbase",
+        image: "https://skylab.rocketseat.com.br/static/0828532024cb46921a6b5e941f8d788d.svg",
+        title: "LauncBase",
+        modules:"16 Módulos",
+        price:"$"
+    },
+    {
+        id: "gostack",
+        image: "https://skylab.rocketseat.com.br/static/83a178a0653dab1d55e2ed7946465975.svg",
+        title: "GoStack",
+        modules:"32 Módulos",
+        price:"$$"
+    }
+]
